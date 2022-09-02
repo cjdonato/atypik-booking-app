cd8b09718f8ff7ae3365c553c3783995c5f0d4d0
CJ <cj.donato10@gmail.com>
2022-09-03 01:37:03 +0800

    Search modif

--- components/Search.vue
+++ components/Search.vue
@@ -1,11 +1,17 @@
 <template>
   <!-- <div class="flex flex-col items-center px-24 py-8 w-screen bg-green-800"> -->
   <div class="flex flex-col items-center px-24 py-8 w-screen">
-    <input
+    <!-- <input
       type="text"
       placeholder="Quel type d'habtitat souhaitez-vous louer"
       class="input input-bordered w-10/12 m-2"
-    />
+    /> -->
+    <select v-model="type" class="select select-bordered w-10/12 m-2" required>
+      <option disabled selected>Quel type d'habtitat souhaitez-vous louer</option>
+      <option >Category1</option>
+      <option>Category2</option>
+      <option>Category3</option>
+    </select>
     <input
       type="text"
       placeholder="Où souhaitez-vous vous évader"
@@ -21,10 +27,11 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
+<script>
+export default {
   name: 'SearchComponent',
-})
+  data: () => ({
+    type: "Quel type d'habtitat souhaitez-vous louer",
+  }),
+}
 </script>
